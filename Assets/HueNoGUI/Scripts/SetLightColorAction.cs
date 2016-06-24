2fadff5ddd24a86e5b110392ccb86ccf42d1f214
Philippe Lewicki <philippe@ofuz.org>
2016-06-24 14:20:50 -0700

    First working version
    Username and ip of hub are hardcoded in the sample

---
+++ Assets/HueNoGUI/Scripts/SetLightColorAction.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetLightColorAction : MonoBehaviour {
+
+    public Color color;
+    [Tooltip("GameObject with the HueBridgeManager script")]
+    public GameObject HueBridge_gameobject;
+    private HueBridgeManager hueBridge;
+    private List<HueLamp> lamps;
+    private HueLamp lamp;
+
+    // Use this for initialization
+    void Start () {
+        Renderer rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Specular");
+        rend.material.SetColor("_SpecColor", color);
+        rend.material.SetColor("_Color", color);
+        //gameObject.GetComponent<Renderer>().material.color = color;
+        hueBridge = HueBridge_gameobject.GetComponent<HueBridgeManager>();
+
+    }
+	
+    void OnSelect ()
+    {
+        HueLamp[] lamps = hueBridge.GetComponentsInChildren<HueLamp>();
+        foreach (HueLamp lamp in lamps)
+        {
+            //Debug.Log("lamp before: " + lamp.devicePath + " : " + lamp.on);
+
+            // Get the second lamp using device path.
+            if (lamp.devicePath == "2")
+            {
+                    lamp.on = true;
+                    lamp.color = color;
+            }
+
+            //Debug.Log("lamp after: " + lamp.devicePath + " : " + lamp.on);
+
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
---
+++ Assets/Scripts/DisplayLightControls.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayLightControls : MonoBehaviour {
+
+    private GameObject controls;
+	// Use this for initialization
+    
+	void Start () {
+        controls = GameObject.Find("lampControls");
+        Debug.Log("Controls status on start:" + controls.activeSelf);
+        controls.SetActive(false);
+
+    }
+	
+    void OnSelect ()
+    {
+
+        if (controls.activeSelf == false)
+        {
+            controls.SetActive(true);
+        } else
+        {
+            controls.SetActive(false);
+        }
+        
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            OnSelect();
+
+        }
+
+    }
+}
